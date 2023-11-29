dedd33a406b47342e7bbd707087297e19bd779cd
sahil-sagwekar2652 <sahilss2652@gmail.com>
2023-11-29 17:57:54 +0530

    cleanup backup.sh

--- db/backup.sh
+++ db/backup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Define variables
-DB_NAME="wordpress"
-
-# Set the current date for the dump file
-DATE=$(date +"%Y%m%d_%H%M%S")
-DUMP_FILE="/etc/mysql/conf.d/backup/${DB_NAME}_backup_$DATE.sql"
-
 # Run mysqlbackup command
 mariabackup --defaults-extra-file=/home/bro.cnf \
    --backup \
